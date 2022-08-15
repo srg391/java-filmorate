ec6915d4f2286c00ae729115e0710ce15d307e0e
Sergei <srg391@yandex.ru>
2022-08-15 04:14:00 +0300

    add-database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,9 +4,9 @@ merge into ratings key (rating_id, rating) values (3, 'PG-13');
 merge into ratings key (rating_id, rating) values (4, 'R');
 merge into ratings key (rating_id, rating) values (5, 'NC-17');
 
-merge into genres key (id, name) values (1, '�������');
-merge into genres key (id, name) values (2, '�����');
-merge into genres key (id, name) values (3, '����������');
-merge into genres key (id, name) values (4, 'Мультфильм');
+merge into genres key (id, name) values (1, 'Комедия');
+merge into genres key (id, name) values (2, 'Драма');
+merge into genres key (id, name) values (3, 'Мультфильм');
+merge into genres key (id, name) values (4, 'Триллер');
 merge into genres key (id, name) values (5, 'Документальный');
 merge into genres key (id, name) values (6, 'Боевик');
